fcb212c9d0054dd2136f0d7478e8a559cd3268d1
Nick Gronow <nick@wov.io>
2019-04-08 15:36:41 -0400

    Only run db:migrate on rails container

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -4,18 +4,19 @@
 echo "Entrypoint starting directory: $(pwd)"
 echo "Rails environment: $RAILS_ENV"
 
-# Database migrations
-echo "Running db:migrate"
-bundle exec rails db:migrate
-if [[ $? != 0 ]]; then
-  echo "== Failed to migrate. Running setup first."
-  bundle exec rails db:setup && bundle exec rails db:migrate
-fi
-
 # Sidekiq
 if [[ $@ = 'sidekiq' ]]; then
   bundle exec sidekiq
 else
+
+  # Database migrations
+  echo "Running db:migrate"
+  bundle exec rails db:migrate
+  if [[ $? != 0 ]]; then
+    echo "== Failed to migrate. Running setup first."
+    bundle exec rails db:setup && bundle exec rails db:migrate
+  fi
+
   # Rails
   echo "Precompile assets"
   bundle exec rails assets:precompile
