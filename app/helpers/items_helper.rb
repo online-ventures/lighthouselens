a0a1af01094ddfb156bfc1f91e8fd9c12b77726c
Nick Gronow <ngronow@lwolf.com>
2022-11-06 13:47:58 -0500

    Add sold price support

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,5 +1,7 @@
 module ItemsHelper
   def pretty_price(price)
+    return 'SOLD' if price == 9999
+
     return "$#{number_with_delimiter(price, delimiter: ',')}" if price.to_i.positive?
 
     'Call for pricing'
